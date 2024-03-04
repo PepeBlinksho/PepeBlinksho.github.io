051f01ad42c7b349d6b8e41f925cae1d5c1df3d8
Sho Ikeda <tenibaka333@gmail.com>
2024-03-04 15:56:09 +0900

    feature: test用関数実装

---
+++ loader.js
@@ -0,0 +1,18 @@
+// とりあえず動かす用
+function addScript(src, attr) {
+  var e = document.createElement('script');
+  e.src = src;
+  if (attr) {for (var i in attr) {e.setAttribute(i, attr[i]);}}
+
+  var f = function() {
+      var bodyElm = document.getElementsByTagName('body').item(0);
+      if (bodyElm) {
+          bodyElm.appendChild(e);
+      } else {
+          window.setTimeout(f, 100);
+      }
+  };
+  f();
+}
+
+addScript('https://pepeblinksho.github.io/app.js')
