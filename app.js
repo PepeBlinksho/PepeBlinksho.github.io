42598d16bfae196a37b4448f9f4ebbd57742e2b2
Sho Ikeda <tenibaka333@gmail.com>
2024-03-04 16:38:39 +0900

    1

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const createDom = () => {
   const body = document.querySelector('body')
   console.log(body)
   body.appendChild(e)
+  let relationData = localStorage.getItem("messageWindow/call");
+  console.log(relationData)
+
+  localStorage.setItem('test000000000000', 'ABC123');
+  let widgetData = localStorage.getItem("test000000000000");
+  console.log(widgetData)
 }
 
 const main = () => {
