e078b23da255900e333b8b042964c452128a79ae
Sho Ikeda <tenibaka333@gmail.com>
2024-03-04 16:17:34 +0900

    chore: defer検証

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ console.log('HELLO!')
 const createDom = () => {
   const e = document.createElement('div');
   e.id = 'relation-widget'
+  console.log(e)
   const body = document.querySelector('body')
+  console.log(body)
   body.appendChild(e)
 }
 
